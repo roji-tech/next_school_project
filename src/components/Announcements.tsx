7fbf0c57e07d7b35a45f689cef23fd0f7b27dd15
roji-tech <rojitech9@gmail.com>
2025-01-01 18:19:54 +0100

    teachers page

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 const Announcements = () => {
   return (
     <div className="bg-white p-4 rounded-md">
       <div className="flex items-center justify-between">
         <h1 className="text-xl font-semibold">Announcements</h1>
-        <span className="text-xs text-gray-400">View All</span>
+        <Link href="/list/announcements" className="flex items-center gap-2">
+          <span className="text-xs text-gray-400">View All</span>
+        </Link>
       </div>
       <div className="flex flex-col gap-4 mt-4">
         <div className="bg-schSkyLight rounded-md p-4">
